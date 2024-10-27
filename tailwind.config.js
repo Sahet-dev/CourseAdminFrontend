a385d3dc5a22f2c7555be601fc028967bac34f5a
Sahetmyrat <kakajansahy@gmail.com>
2024-10-27 11:01:27 +0200

    Fix design issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     content: [
         './index.html',
         './src/**/*.{vue,js,ts,jsx,tsx}',
+        './dist/**/*.{html,vue,js}',
     ],
     theme: {
         extend: {},
