cfa8b0d689e0ffb715404c3a32ab5f76af9dacad
Sahetmyrat <kakajansahy@gmail.com>
2024-11-05 09:57:43 +0200

    prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
+
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     base: './',
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            'vue': 'vue'
+        }
+    },
     build: {
         outDir: 'dist',
-    },
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
+    }
 })
